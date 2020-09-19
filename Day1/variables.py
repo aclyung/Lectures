845da258bf03e656c350efc24c79bdd39ddffe23
aclyung <aclyung1234556@gmail.com>
2020-09-19 12:13:15 +0900

    commit

--- Day1/variables.py
+++ Day1/variables.py
@@ -1,11 +1,10 @@
-def sum(x,y):
-    return x+y
-    
 a = 100
 b = 50
-print(a)
-print(b)
-c=a+b
-print(c, a+b)
-print(sum(c,b))
-print(a,'-', b)
+res = a + b
+print(a, '+', b, '=' , res)
+res = a - b
+print(a, '-', b, '=' , res)
+res = a * b
+print(a, '*', b, '=' , res)
+res = a / b
+print(a, '/', b, '=' , res)
