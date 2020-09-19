a74c62cce857b1d03d497ebf2dcbe9c2d4a04923
aclyung <aclyung1234556@gmail.com>
2020-09-19 12:24:07 +0900

    comiit

--- Day1/variables.py
+++ Day1/variables.py
@@ -1,5 +1,5 @@
-a = int(input("please insert value of variable a:\n"))
-b = int(input("please insert value of variable b:\n"))
+a = int(input("please enter value of variable a:\n"))
+b = int(input("please enter value of variable b:\n"))
 res = a + b
 print(a, '+', b, '=' , res)
 res = a - b
