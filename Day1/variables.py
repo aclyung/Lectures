f66fab924194074a74c088e8ffea010dd41de870
aclyung <aclyung1234556@gmail.com>
2020-09-19 12:35:01 +0900

    commit

--- Day1/variables.py
+++ Day1/variables.py
@@ -1,10 +1,7 @@
-a = int(input("please enter value of variable a:\n"))
-b = int(input("please enter value of variable b:\n"))
-res = a + b
-print(a, '+', b, '=' , res)
-res = a - b
-print(a, '-', b, '=' , res)
-res = a * b
-print(a, '*', b, '=' , res)
-res = a / b
-print(a, '/', b, '=' , res) 
+a = int(input("please enter a value for variable a:\n"))
+b = int(input("please enter a value for variable b:\n"))
+res = [a + b, a-b, a*b, a/b]
+opr = ['+', '-', '*', '/']
+for c,o in zip(res,opr):
+    print(a,o,b,c)
+
