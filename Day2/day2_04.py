a0f365e989d39cdb1449191355499cc6d49156b2
aclyung <aclyung1234556@gmail.com>
2020-09-20 13:08:28 +0900

    commit

---
+++ Day2/day2_04.py
@@ -0,0 +1,11 @@
+a, b = 100, 200
+print(a == b, a!= b, a >b, a<b, a>=b, a<=b)
+print(not(a==b and a>b))
+if 1234:
+    print("True")
+
+if 0:
+    print("False")
+else:
+    if 1234:
+        print("True")
---
+++ Day2/day2_05.py
@@ -0,0 +1,3 @@
+americano_price, caffelatte_price, capucino_price = 2000, 3000, 3500
+americano, caffelatte, capucino = int(input("아메리카노 판매갯수: ")), int(input("카페라테 판매갯수: ")), int(input("카푸치노 판매개수: "))
+print("총 매출액은", americano*americano_price + caffelatte_price*caffelatte+ capucino*capucino_price,"입니다" )
