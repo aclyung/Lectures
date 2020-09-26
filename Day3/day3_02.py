9074101a703ff6eb163001d1f9e0eb1758b0bfcf
aclyung <aclyung1234556@gmail.com>
2020-09-26 09:29:13 +0900

    comiit

--- Day3/day3_02.py
+++ Day3/day3_02.py
@@ -1,3 +1,3 @@
 weight = int(input("몸무게(kg): "))
-height = int(input("키(cm): "))
-print("BMI는 %.2f (kg/m^2) 입니다" %(weight/((height/100)**2)))
+height = int(input("키(cm): "))/100
+print("BMI는 %.2f (kg/m^2) 입니다" %(weight/(height**2)))
